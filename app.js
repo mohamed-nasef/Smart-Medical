58e2beb38041b70200ab39838de9cea0c3d8de0a
ay862 <ayhaidy226@gmail.com>
2022-06-25 03:25:47 +0200

    add doctor and nurse in app.js

--- app.js
+++ app.js
@@ -4,11 +4,15 @@ const express = require("express");
 const app = express();
 var port = process.env.PORT || 8080;
 const patient = require("./routers/patientRouters");
+const doctor = require("./routers/doctorRouters");
+const nurse = require("./routers/nurseRouters");
 const myMongoose=require('mongoose');
 const connectionString="mongodb+srv://smartmedic:smartmedic2022@smartmedic.r5ddgad.mongodb.net/smartmedical?retryWrites=true&w=majority"
 app.use(cors());
 app.use(bodyParser.json());
 app.use("/patient",patient);
+app.use("/doctor",doctor);
+app.use("/nurse",nurse);
 
 
 myMongoose.connect(connectionString,()=>{
