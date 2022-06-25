d4d409eea5d13df78792cd9ba71a5e10b41cde50
mohamed-nasef <mnasef82@gmail.com>
2022-06-25 21:20:06 +0200

    add doctor and nurse schedule

--- app.js
+++ app.js
@@ -1,23 +1,30 @@
 const cors =require('cors');
 const bodyParser = require('body-parser');
 const express = require("express");
+const myMongoose=require('mongoose');
+const connectionString="mongodb+srv://smartmedic:smartmedic2022@smartmedic.r5ddgad.mongodb.net/smartmedical?retryWrites=true&w=majority"
 const app = express();
 var port = process.env.PORT || 8080;
 const patient = require("./routers/patientRouters");
 const doctor = require("./routers/doctorRouters");
 const nurse = require("./routers/nurseRouters");
 const dPatientSchedule = require("./routers/docPatScheduleRouters");
-const nPatientSchedule = require("./routers/nurPatScheduleRouters"); 
-const myMongoose=require('mongoose');
-const connectionString="mongodb+srv://smartmedic:smartmedic2022@smartmedic.r5ddgad.mongodb.net/smartmedical?retryWrites=true&w=majority"
+const nPatientSchedule = require("./routers/nurPatScheduleRouters");
+const nurseSchedule = require("./routers/nurseScheduleRouters");
+const doctorSchedule = require("./routers/doctorScheduleRouters");
+
 
 app.use(cors());
 app.use(bodyParser.json());
+
 app.use("/patient",patient);
 app.use("/doctor",doctor);
 app.use("/nurse",nurse);
 app.use("/dpatient",dPatientSchedule);
 app.use("/npatient",nPatientSchedule);
+app.use("/nurseschedule",nurseSchedule);
+app.use("/doctorschedule",doctorSchedule);
+
 
 
 myMongoose.connect(connectionString,()=>{
