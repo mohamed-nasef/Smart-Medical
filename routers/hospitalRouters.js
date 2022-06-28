8e5a99f3eaf04a7a91c0b3dcf67306af7e493fa5
ay862 <ayhaidy226@gmail.com>
2022-06-28 23:16:12 +0200

    comit

---
+++ routers/hospitalRouters.js
@@ -0,0 +1,46 @@
+const Hospital = require("../Modules/hospitalModules");
+const Doctor = require("../Modules/doctorModules");
+const Nurse = require("../Modules/nurseModules");
+const express = require("express");
+const hospitalRouter = express.Router();
+
+ hospitalRouter.get("/", async (req,res)=>{
+    const doctor_Schedule = await Hospital.aggregate([
+        {
+          $lookup:
+            {
+              from: "doctorschedules",
+              localField: "_id",
+              foreignField: "doctorID",
+              as: "doctor_Schedule"
+            }
+       }
+     ])
+    try{
+        res.json(doctor_Schedule);
+    } catch(error)
+    {
+        console.log(error);
+    }
+    
+});
+
+hospitalRouter.post("/",async (req,res)=>{
+    const doctor = new doctorSchedule({
+        "day" : req.body.day,
+        "from" : req.body.from,
+        "to" : req.body.to,
+        "doctorID" : req.body.doctorID
+        })
+       try{
+        const doctorData = await doctor.save()
+        res.json({
+            "message":"doctor Schedule created successfully",
+            "data":doctorData
+        })
+    } 
+    catch (error) {
+        console.log(error);
+    }
+});
+module.exports = hospitalRouter;
