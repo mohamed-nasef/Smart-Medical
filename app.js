7fc048d8e007080e46a3eba005200a0531a83d34
mohamed-nasef <mnasef82@gmail.com>
2022-06-30 18:52:10 +0200

    try to fix get picture

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(bodyParser.json());
 app.use(express.json());
 
 
-app.use("/photo/:filename", async function(req,res){
+app.use("/photo/:filename", function(req,res){
   fs.readFile(`./uploads/${req.params.filename}`, async (err, data)=>{
       if (err) throw err; // Fail if the file can't be read.
       try {
